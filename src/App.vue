71aaa4b49fb7b73b1935fe132def98fac6ef574f
DANTES0 <kzstar18rus@gmail.com>
2024-03-06 10:36:28 +0300

    123

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import  NavBar from './components/Header.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <NavBar>
+  </NavBar>
+  <div class="main-background">
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+* {
+  background-color: #151A31;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+.main-background{
+        width: auto;
+        height: auto;
+
+    }
+    .text-vo {
+      width: 100%;
+      max-width: 1336px;
+      height: 400px;
+    }
 </style>
