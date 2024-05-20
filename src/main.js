d9b2aae1f43d0a4db2169683d922e0463ba3a7cf
Bondike <bondike983@gmail.com>
2024-05-20 15:40:39 +0300

    Импортировал vite-plugin-vuetify

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import router from './router/routes'
 import App from './App.vue'
 import './style.css'
+import vuetify from './plugins/vuetify';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import { VPagination } from 'vuetify/components';
+
+export default createVuetify({
+  components: {
+    VPagination,
+  },
+});
