dea801a80e2548d27208667a7bf0b8ff4e2575d5
Bondike <bondike983@gmail.com>
2024-05-21 23:29:00 +0300

    Добавил пропсы в компоненты отдельной новости

--- src/components/NewsContentPageComponents/NewsContentPageSlider.vue
+++ src/components/NewsContentPageComponents/NewsContentPageSlider.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, shallowReactive } from 'vue';
+import { ref, shallowReactive, computed, defineProps } from 'vue';
 import {
   VueFlux,
   FluxCaption,
@@ -8,11 +8,22 @@ import {
   FluxPagination,
   FluxPreloader,
   Img,
-  Book,
-  Zip,
   Slide,
 } from 'vue-flux';
 import 'vue-flux/style.css';
+
+// Импортируем изображения
+import pivo1 from "../../assets/NewsContent/pivo1.jpg";
+import pivo2 from "../../assets/NewsContent/pivo2.jpg";
+import pivo3 from "../../assets/NewsContent/pivo3.jpg";
+
+// Определяем пропсы для компонента с значением по умолчанию
+const newsSliderImages = defineProps({
+  newsImages: {
+    type: Array,
+    default: () => [pivo1, pivo2, pivo3]
+  }
+});
 
 const transitions = shallowReactive([
   {
@@ -30,65 +41,52 @@ const vfOptions = shallowReactive({
   autoplay: true,
 });
 
-import pivo1 from "../../assets/NewsContent/pivo1.jpg";
-import pivo2 from "../../assets/NewsContent/pivo2.jpg";
-import pivo3 from "../../assets/NewsContent/pivo3.jpg";
+// Создаем массив ресурсов из пропсов
+const vfRscs = computed(() => newsSliderImages.newsImages.map(image => new Img(image)));
 
-
-const vfRscs = shallowReactive([
-      new Img(pivo1),
-    new Img(pivo2),
-new Img(pivo3),
-]);
-
-// const vfTransitions = shallowReactive([Book, Zip]);
 </script>
 
 <template>
-
   <div class="photo-slider">
     <VueFlux
-        :options="vfOptions"
-        :rscs="vfRscs"
-        :transitions="transitions"
-        ref="$vueFlux">
-
+      :options="vfOptions"
+      :rscs="vfRscs"
+      :transitions="transitions"
+      ref="$vueFlux"
+    >
       <template #preloader="preloaderProps">
-        <FluxPreloader v-bind="preloaderProps"/>
+        <FluxPreloader v-bind="preloaderProps" />
       </template>
 
       <template #caption="captionProps">
-        <FluxCaption v-bind="captionProps"/>
+        <FluxCaption v-bind="captionProps" />
       </template>
 
       <template #controls="controlsProps">
-        <FluxControls v-bind="controlsProps"/>
+        <FluxControls v-bind="controlsProps" />
       </template>
 
       <template #pagination="paginationProps">
-        <FluxPagination v-bind="paginationProps"/>
+        <FluxPagination v-bind="paginationProps" />
       </template>
 
       <template #index="indexProps">
-        <FluxIndex v-bind="indexProps"/>
+        <FluxIndex v-bind="indexProps" />
       </template>
     </VueFlux>
-
   </div>
 
   <!--  <button @click="$vueFlux.show('next')">NEXT</button>-->
 </template>
 
 <style scoped>
-
 :deep(.visible ul) {
   background-color: #000000c7;
-
 }
 :deep(.visible li) {
   opacity: 1 !important;
 }
-.photo-slider{
+.photo-slider {
   width: 70%;
 }
 
