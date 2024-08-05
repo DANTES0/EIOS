cf185d9b95a2152f588a82f19710638fcc1d68dd
DANTES0 <kzstar18rus@gmail.com>
2024-08-05 12:19:24 +0400

    fix: Тема меняется на другом устройстве

--- src/App.vue
+++ src/App.vue
@@ -28,9 +28,11 @@ onMounted(() => {
         (!('theme' in localStorage) &&
             window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
+        localStorage.theme = 'dark';
         document.documentElement.classList.add('dark');
     } else {
         document.documentElement.classList.remove('dark');
+        localStorage.theme = 'light';
     }
 });
 </script>
